a2ef50286449a7aef9f91ec1c9ebc0def24c1604
Robbie Page <rob@rorpage.com>
2016-06-16 16:18:06 -0400

    add Newtonsoft, download Centova data, return more data

--- wdwnt-ota-api/Modules/ApiModule.cs
+++ wdwnt-ota-api/Modules/ApiModule.cs
@@ -1,4 +1,6 @@
-﻿using Nancy;
+﻿using System.Net;
+using Nancy;
+using Newtonsoft.Json.Linq;
 
 namespace wdwnt_ota_api.Modules
 {
@@ -6,7 +8,15 @@ namespace wdwnt_ota_api.Modules
     {
         public ApiModule() : base("/api/v1")
         {
-            Get["/info"] = _ => Response.AsJson(new { Status = true });
+            Get["/info"] = _ =>
+            {
+                var webClient = new WebClient();
+                var centovaJson = webClient.DownloadString("http://panel2.directhostingcenter.com:2199/rpc/wukcrjdg/streaminfo.get");
+                return Response.AsJson(new
+                {
+                    Centova = JObject.Parse(centovaJson)
+                });
+            };
         }
     }
 }

6756ff29a20a5fda899889c9286d03507ac50791
Robbie Page <rob@rorpage.com>
2016-06-16 16:11:41 -0400

    initial commit

---
+++ wdwnt-ota-api/ApplicationStartup.cs
@@ -0,0 +1,16 @@
+﻿using Nancy.Bootstrapper;
+
+namespace wdwnt_ota_api
+{
+    public class ApplicationStartup : IApplicationStartup
+    {
+        public void Initialize(IPipelines pipelines)
+        {
+            pipelines.AfterRequest += ctx =>
+            {
+                ctx.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+                ctx.Response.Headers.Add("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+            };
+        }
+    }
+}
---
+++ wdwnt-ota-api/Modules/ApiModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace wdwnt_ota_api.Modules
+{
+    public class ApiModule : NancyModule
+    {
+        public ApiModule() : base("/api/v1")
+        {
+            Get["/info"] = _ => Response.AsJson(new { Status = true });
+        }
+    }
+}
